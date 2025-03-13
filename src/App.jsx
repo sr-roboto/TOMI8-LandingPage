cb222f05998d4a42767b1d0e2a9b7ca3f4a6b57f
sr-roboto <marcoslopez123.lu@gmail.com>
2025-03-13 20:51:28 -0300

    Agregar configuración inicial de React y Vite con Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>Hola mundo</h1>
+      <svg
+        className="bg-red-500"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+      ></svg>
+    </>
+  );
+}
+
+export default App;
