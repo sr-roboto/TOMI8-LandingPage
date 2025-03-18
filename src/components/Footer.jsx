fd0556d689daeffddf8ddaf0e59de2604e4b4984
sr-roboto <marcoslopez123.lu@gmail.com>
2025-03-18 10:28:26 -0300

    Agregar enlace a video de presentación de TOMIi8 en el pie de página para mejorar la comunicación visual

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,13 @@ function Footer() {
       {/* Footer */}
       <footer className=" bg-[#22233A]">
         <div className="px-3 py-8 md:px-0 md:pt-12 md:pb-8 max-w-[1110px] w-full m-auto">
+          <a
+            href="https://youtu.be/TJ2V5rNVxCY?si=Hg0YvXCy57YKcDgK"
+            target="_blank"
+            className="flex justify-center text-2xl text-green-600 font-bold underline hover:text-green-700"
+          >
+            Ver Video Presentación TOMIi8
+          </a>
           <img src={Tomi8Banner} alt="" />
         </div>
       </footer>
